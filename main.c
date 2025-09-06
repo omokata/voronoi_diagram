ab10338da6c7239960ecc06ee0d62f1e3c96a8bd
dedNewGuy <abeamin23@gmail.com>
2025-09-07 00:54:54 +0800

    render red image in ppm format

--- main.c
+++ main.c
@@ -4,14 +4,24 @@
 #define IMAGE_WIDTH 400
 #define IMAGE_HEIGHT 400
 
-FILE* generate_image_with_bytes(int img_width, int img_height, uint8_t content[img_height][img_width], char *filename)
+#define COLOR_RED 0xFF0000FF
+
+FILE* save_image_as_ppm(int img_width, int img_height, uint32_t image[img_height][img_width], char *filename)
 {
 	FILE *img_file = fopen(filename, "wb");
 
-	fprintf(img_file, "P6\n%d %d\n255\n", img_width, img_height);
-
+	fprintf(img_file, "P6\n%d %d 255\n", img_width, img_height);
 	for (int i = 0; i < img_height; ++i) {
-		fwrite(content[i], sizeof(uint32_t), img_width, img_file);
+		for (int j = 0; j < img_width; ++j) {
+			// 0xAABBGGRR
+			uint32_t pixel = image[i][j];
+			uint8_t bytes[3] = {
+				(pixel&0x0000FF)>>8*0,
+				(pixel&0x00FF00)>>8*1,
+				(pixel&0xFF0000)>>8*2,
+			};
+			fwrite(bytes, sizeof(bytes), 1, img_file);
+		}
 	}
 	return img_file;
 }
@@ -19,14 +29,14 @@ FILE* generate_image_with_bytes(int img_width, int img_height, uint8_t content[i
 int main(void)
 {
 	char *output_file = "output.ppm";
-	uint8_t image_content[IMAGE_HEIGHT][IMAGE_WIDTH];
+	uint32_t image[IMAGE_HEIGHT][IMAGE_WIDTH];
 	for (int i = 0; i < IMAGE_HEIGHT; ++i) {
 		for (int j = 0; j < IMAGE_WIDTH; ++j) {
-			uint8_t color = 0x00;
-			image_content[i][j] = color;
+			uint32_t color = COLOR_RED;
+			image[i][j] = color;
 		}
 	}
-	FILE *img_file = generate_image_with_bytes(IMAGE_WIDTH, IMAGE_HEIGHT, image_content, output_file);
+	FILE *img_file = save_image_as_ppm(IMAGE_WIDTH, IMAGE_HEIGHT, image, output_file);
 	fclose(img_file);
 	return 0;
 }
